87b3a4d295323cc308f3a4f58f864cf56e0979a9
Benes Karel <ibenes@fit.vutbr.cz>
2024-01-01 22:31:33 +0100

    Update CLI

--- textbite/user_scripts/infer.py
+++ textbite/user_scripts/infer.py
@@ -26,12 +26,12 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--logging-level", default='WARNING', choices=['ERROR', 'WARNING', 'INFO', 'DEBUG'])
-    parser.add_argument("--data", required=True, type=str, help="Path to a folder with xml data.")
+    parser.add_argument("--xml-input", required=True, type=str, help="Path to a folder with xml data of transcribed pages.")
     parser.add_argument("--images", required=True, type=str, help="Path to a folder with images data.")
     parser.add_argument("--altos", type=str, help="Path to a folder with alto data.")
-    parser.add_argument("--xml-output", type=str, help="Where to put reorganized PAGE XMLs.")
+    parser.add_argument("--xml-output", type=str, required=True, help="Where to put reorganized PAGE XMLs.")
     parser.add_argument("--model", required=True, type=str, help="Path to the .pt file with weights of YOLO model.")
-    parser.add_argument("--save", required=True, type=str, help="Folder where to put output jsons.")
+    parser.add_argument("--bites-out", type=str, help="Folder where to put output TextBites as raw jsons.")
 
     return parser.parse_args()
 
@@ -175,11 +175,13 @@ def main():
     biter = YoloBiter(YOLO(args.model))
     xml_enhancer = PageXMLEnhancer()
 
-    os.makedirs(args.save, exist_ok=True)
-    xml_filenames = [xml_filename for xml_filename in os.listdir(args.data) if xml_filename.endswith(".xml")]
+    if args.bites_out:
+        os.makedirs(args.bites_out, exist_ok=True)
+
+    xml_filenames = [xml_filename for xml_filename in os.listdir(args.xml_input) if xml_filename.endswith(".xml")]
 
     for filename in xml_filenames:
-        path_xml = os.path.join(args.data, filename)
+        path_xml = os.path.join(args.xml_input, filename)
         layout = PageLayout()
         with open(path_xml) as f:
             layout.from_pagexml(f)
@@ -190,12 +192,13 @@ def main():
         logging.info(f"Processing: {path_xml}")
         bites = biter.produce_bites(path_img, layout, path_alto)
 
-        out_path = os.path.join(args.save, filename.replace(".xml", ".json"))
-        save_result(bites, out_path)
+        if args.bites_out:
+            out_path = os.path.join(args.bites_out, filename.replace(".xml", ".json"))
+            save_result(bites, out_path)
 
         try:
             out_xml_string = xml_enhancer.process(layout, bites)
-            out_path = os.path.join(args.save, filename)
+            out_path = os.path.join(args.xml_output, filename)
             with open(out_path, 'w', encoding='utf-8') as out_f:
                 out_f.write(out_xml_string)
         except UnsupportedLayoutError as e:
